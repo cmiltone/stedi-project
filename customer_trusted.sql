95894dbf437e86713c36fec739373fa9e3f9d34f
Castro Hamiltone <castromiltone@gmail.com>
2023-11-26 19:58:00 +0300

    adds customer_trusted.sql

--- customer_trusted.sql
+++ customer_trusted.sql
@@ -1,3 +1,12 @@
+CREATE EXTERNAL TABLE `customer_trusted`(
+  `customername` string,
+  `email` string,
+  `birthday` string,
+  `serialnumber` string,
+  `sharewithresearchasofdate` bigint)
+LOCATION
+  's3://udacity-spark-bucket/customer/trusted'
+
 INSERT INTO "stedi"."customer_trusted" (customername, email, birthday, serialnumber, sharewithresearchasofdate)
 SELECT customername, email, birthday, serialnumber, sharewithresearchasofdate
 FROM "stedi"."customer_landing"
